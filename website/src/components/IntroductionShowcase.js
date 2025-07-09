6c2335c1cabb78e3b96097490d9bc6543af27659
AM-XIX <am.lannaud@gmail.com>
2025-07-09 19:00:44 +0200

    Add cards section

--- website/src/components/IntroductionShowcase.js
+++ website/src/components/IntroductionShowcase.js
@@ -27,10 +27,28 @@ export default function IntroductionShowcase() {
 
         <YoutubeVideo id="jsjkjCQCPno" />
 
-         <h2 className={styles.sectionTitle}>
-         Coollab, the software you’ve been looking for
-        </h2>
-        <a className={styles.primaryBtn} href="#">Download</a>
+        <div className={styles.cardSection}>
+          <h2 className={styles.sectionTitle}>
+            Coollab, the software you’ve been looking for
+          </h2>
+
+          <div className={styles.cardContainer}>
+            <div className={styles.card} style={{ borderColor: '#F4AD7F' }}>
+              <div className={styles.cardImage}></div>
+              <p className={styles.cardText}>Create cool visuals with no <span style={{color:'#F4AD7F'}}>code</span> in no time</p>
+            </div>
+            <div className={styles.card} style={{ borderColor: '#8656D7' }}>
+              <div className={styles.cardImage}></div>
+              <p className={styles.cardText}>Free and <span style={{color:'#8656D7'}}>Open-source</span> software</p>
+            </div>
+            <div className={styles.card} style={{ borderColor: '#30A7F5' }}>
+              <div className={styles.cardImage}></div>
+              <p className={styles.cardText}>A powerful tool for <span style={{color:'#30A7F5'}}>Vjing</span> and so much more!</p>
+            </div>
+          </div>
+
+          <a className={styles.primaryBtn} href="#">Download</a>
+        </div>
       </div>
     </section>
   );
