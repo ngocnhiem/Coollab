caf690baf2e9cadef86254b14b2f57fdf2df1ac2
AM-XIX <am.lannaud@gmail.com>
2025-07-11 20:58:25 +0200

    OS section

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from '@theme/Layout';
 import styles from './index.module.css';
 import IntroductionShowcase from '../components/IntroductionShowcase';
 import Timeline from '../components/Timeline';
+import { FaWindows, FaApple, FaLinux } from 'react-icons/fa';
 
 export default function Home() {
   return (
@@ -22,6 +23,31 @@ export default function Home() {
       </main>
       <IntroductionShowcase />
       <Timeline />
+      <div className={styles.osSection}>
+        <h2 className={styles.sectionTitle}>
+          Get Coollab now
+        </h2>
+        <p className={styles.sectionSubtitle}>
+          Select the OS of your choice.
+        </p>
+<div className={styles.osButtons}>
+  <a className={`${styles.secondaryBtn} ${styles.osButton}`}  href="#">
+    <FaWindows className={styles.osIcon} />
+    Windows
+  </a>
+  <a className={`${styles.secondaryBtn} ${styles.osButton}`} href="#">
+    <FaApple className={styles.osIcon} />
+    macOS
+  </a>
+  <a className={`${styles.secondaryBtn} ${styles.osButton}`} href="#">
+    <FaLinux className={styles.osIcon} />
+    Linux
+  </a>
+</div>
+<p className={styles.osSubtitle}>
+  This software uses code of <a href="https://ffmpeg.org/" target="_blank" rel="noopener noreferrer">FFmpeg</a> licensed under the <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" rel="noopener noreferrer">GPLv3.0</a> and its source can be downloaded <a href="https://github.com/Coollab-Art/Coollab" target="_blank" rel="noopener noreferrer">here</a>.
+</p>
+      </div>
     </Layout>
   );
 }
