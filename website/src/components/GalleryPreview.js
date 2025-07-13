93b6d3e99950e3b40693256a21142e6a64ef31ba
AM-XIX <am.lannaud@gmail.com>
2025-07-13 13:35:17 +0200

    Complete Gallery Preview

--- website/src/components/GalleryPreview.js
+++ website/src/components/GalleryPreview.js
@@ -2,22 +2,45 @@ import React from 'react';
 import styles from './GalleryPreview.module.css';
 
 const images = [
-  '/img/gallery/img1.jpg',
-  '/img/gallery/img2.jpg',
-  '/img/gallery/img3.jpg',
-  '/img/gallery/img4.jpg',
-  '/img/gallery/img5.jpg',
-  '/img/gallery/img6.jpg',
+  '/img/lab/5.png',
+  '/img/lab/6.png',
+  '/img/lab/10.png',
+  '/img/lab/11.png',
+  '/img/lab/7.png',
+  '/img/lab/9.png',
 ];
 
 export default function GalleryPreview() {
+  const rows = [];
+  for (let i = 0; i < images.length; i += 2) {
+    rows.push(images.slice(i, i + 2));
+  }
+
   return (
-    <section className={styles.gallerySection}>
-      <div className={styles.grid}>
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`Artwork ${index + 1}`} className={styles.image} />
-        ))}
+    <>
+      <section className={styles.gallerySection}>
+        {rows.map((pair, rowIndex) => {
+          const isEven = rowIndex % 2 === 0;
+          const firstImage = isEven ? pair[0] : pair[1];
+          const secondImage = isEven ? pair[1] : pair[0];
+
+          return (
+            <div key={rowIndex} className={styles.row}>
+              <div
+                className={`${styles.imageTile} ${isEven ? styles.narrow : styles.wide}`}
+                style={{ backgroundImage: `url(${firstImage})` }}
+              />
+              <div
+                className={`${styles.imageTile} ${isEven ? styles.wide : styles.narrow}`}
+                style={{ backgroundImage: `url(${secondImage})` }}
+              />
+            </div>
+          );
+        })}
+      </section>
+      <div className={styles.buttonWrapper}>
+        <a href="/Gallery"  className={styles.secondaryBtn}>See more</a>
       </div>
-    </section>
+    </>
   );
 }
