a1701d63b38d3123bdcb2681d9acedaa26ec4fdd
AM-XIX <am.lannaud@gmail.com>
2025-07-24 17:53:36 +0200

    Add Contribute page index

---
+++ website/src/pages/Contribute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Contribute from '../components/Contribute';
+
+export default function ContributePage() {
+  return (
+    <Layout title="Contribute to Coollab" description="How to help Coollab grow.">
+      <Contribute />
+    </Layout>
+  );
+}
