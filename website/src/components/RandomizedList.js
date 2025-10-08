776b2c20bdefa0fd5cbbc2fb8ba1d1223232984e
Jules Fouchy <jules.fouchy@ntymail.com>
2025-10-08 10:43:24 +0200

    🌐 [website] Try fix RandomizedList

--- website/src/components/RandomizedList.js
+++ website/src/components/RandomizedList.js
@@ -1,17 +1,26 @@
 import React from "react"
+import BrowserOnly from "@docusaurus/BrowserOnly"
 
-export default function RandomizedList({ elements = [] }) {
-  const shuffled = [...elements].sort(() => Math.random() - 0.5)
+const shuffle = (array) => {
+  return array
+    .map((a) => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map((a) => a[1])
+}
 
+export default function ({ elements }) {
   return (
-    <>
-      {shuffled.map((el, i) =>
-        React.isValidElement(el) ? (
-          React.cloneElement(el, { key: i })
-        ) : (
-          <React.Fragment key={i}>{el}</React.Fragment>
+    <BrowserOnly>
+      {() => {
+        const shuffled = shuffle(elements)
+        return shuffled.map((el, i) =>
+          React.isValidElement(el) ? (
+            React.cloneElement(el, { key: i })
+          ) : (
+            <React.Fragment key={i}>{el}</React.Fragment>
+          )
         )
-      )}
-    </>
+      }}
+    </BrowserOnly>
   )
 }
