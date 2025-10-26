596d23e23ccf6641f49338d5a36ff535cdafd499
Jules Fouchy <jules.fouchy@ntymail.com>
2025-10-26 12:33:04 +0100

    🌐 [website] Responsive: Download page

--- website/src/pages/Download.js
+++ website/src/pages/Download.js
@@ -6,6 +6,7 @@ import SectionTitle from "../components/SectionTitle"
 import Spacer from "../components/Spacer"
 import DonateButton from "../components/DonateButton"
 import JoinDiscordButton from "../components/JoinDiscordButton"
+import { MobileOnly } from "../components/Responsive"
 
 export default function DownloadPage() {
   return (
@@ -22,15 +23,17 @@ export default function DownloadPage() {
 
         {/* Right: Download Section */}
         <main className={style.downloadContent}>
+          <MobileOnly>
+            <Spacer height="1.2rem" />
+          </MobileOnly>
           <SectionTitle fontSize="3.5rem" mobileFontSize="2.5rem">
             Install Coollab
           </SectionTitle>
           <Spacer height="2.4rem" />
           <DownloadButtons />
           <Spacer height="0.8rem" />
-          <div style={{ width: "fit-content" }}>
+          <div style={{ width: "fit-content" }} className={style.extraButtons}>
             <DonateButton />
-            <Spacer height="1.2rem" />
             <JoinDiscordButton />
           </div>
         </main>
